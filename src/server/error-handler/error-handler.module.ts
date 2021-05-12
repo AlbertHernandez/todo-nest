fb11981bd603b0b19659e7bc23144bae17fe1284
Albert Hernandez Pellicer <alberto.pellicer@coverwallet.com>
2021-05-12 21:20:37 +0200

    refactor: improve error handler and tracker modules

--- src/server/error-handler/error-handler.module.ts
+++ src/server/error-handler/error-handler.module.ts
@@ -1,21 +1,19 @@
-import { Logger, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Global, Logger, Module } from '@nestjs/common';
 import { ErrorHandlerService } from './error-handler.service';
 import { ConfigService } from '@nestjs/config';
 import { ErrorHandlerInterceptor } from './error-handler.interceptor';
 import { SentryErrorTrackerService } from 'src/server/error-tracker/sentry-error-tracker/sentry-error-tracker.service';
 import { SentryErrorTrackerModule } from '../error-tracker/sentry-error-tracker/sentry-error-tracker.module';
 import { APP_INTERCEPTOR } from '@nestjs/core';
-import { SentryErrorTrackerMiddleware } from '../error-tracker/sentry-error-tracker/sentry-error-tracker.middleware';
 import { ERROR_TRACKER_SERVICE } from 'src/server/error-tracker/constants';
-import { ALL_ROUTES } from '../routes/routes.constants';
 
+@Global()
 @Module({
   imports: [SentryErrorTrackerModule],
-  exports: [ErrorHandlerService, ErrorHandlerInterceptor],
+  exports: [ErrorHandlerService],
   providers: [
     ErrorHandlerService,
     ConfigService,
-    ErrorHandlerInterceptor,
     {
       provide: ERROR_TRACKER_SERVICE,
       useExisting: SentryErrorTrackerService,
@@ -27,8 +25,4 @@ import { ALL_ROUTES } from '../routes/routes.constants';
     Logger,
   ],
 })
-export class ErrorHandlerModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(SentryErrorTrackerMiddleware).forRoutes(ALL_ROUTES);
-  }
-}
+export class ErrorHandlerModule {}
--- src/server/error-tracker/sentry-error-tracker/sentry-error-tracker.module.ts
+++ src/server/error-tracker/sentry-error-tracker/sentry-error-tracker.module.ts
@@ -1,9 +1,15 @@
-import { Logger, Module } from '@nestjs/common';
+import { Logger, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { SentryErrorTrackerService } from './sentry-error-tracker.service';
 import { ConfigService } from '@nestjs/config';
+import { SentryErrorTrackerMiddleware } from './sentry-error-tracker.middleware';
+import { ALL_ROUTES } from '../../routes/routes.constants';
 
 @Module({
   exports: [SentryErrorTrackerService],
   providers: [SentryErrorTrackerService, Logger, ConfigService],
 })
-export class SentryErrorTrackerModule {}
+export class SentryErrorTrackerModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(SentryErrorTrackerMiddleware).forRoutes(ALL_ROUTES);
+  }
+}
